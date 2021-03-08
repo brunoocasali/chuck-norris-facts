ed48c4afd7b362700f2ec0874f3746a83e33a161
Bruno Casali <brunoocasali@gmail.com>
2021-03-07 21:12:16 -0300

    Improve how facts are generated (improve production API compatibility)

--- app/mirage/factories/fact.js
+++ app/mirage/factories/fact.js
@@ -1,10 +1,26 @@
 import { Factory } from 'miragejs';
-import { lorem } from 'faker';
+import { random } from 'faker';
 
 export default Factory.extend({
   iconUrl: "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
 
-  value() {
-    return lorem.paragraph(4);
+  id() {
+    return random.uuid();
   },
+
+  url() {
+    return `https://api.chucknorris.io/jokes/${this.id}`
+  },
+
+  value() {
+    return random.arrayElement([
+      'Chuck Norris is the reason that the Army had to change their slogan, "An Army of One',
+      'Someone once videotaped Chuck Norris getting pissed off. It was called Walker: Texas Chain Saw Masacre',
+      'When Chuck Norris pokes you on Facebook you can feel it',
+      'coyote vs road runner is just a performance how fast can Chuck Norris bip-iping',
+      "Every time someone types 'Chuck Norris', a gas station explodes",
+      'On a recent trip to Vegas, Chuck Norris won a hand of blackjack with 21 aces.',
+      "Hurricanes are mother nature's way of running from Chuck Norris."
+    ]);
+  }
 });
