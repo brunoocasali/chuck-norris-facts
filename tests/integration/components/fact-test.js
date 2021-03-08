85392a178137dfc145e74839ce8973fbd0e6c575
Bruno Casali <brunoocasali@gmail.com>
2021-03-07 21:45:47 -0300

    Add fact component and chuck norris image
    image from: https://www.pngitem.com/middle/iRTwwTo_transparent-chuck-norris-png-scribblenauts-chuck-norris-png/

---
+++ tests/integration/components/fact-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('FactComponent', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test(`renders data's value`, async function(assert) {
+    this.set('data', { value: 'lorem ipsum' });
+
+    await render(hbs`<Fact @data={{this.data}} />`);
+
+    assert.equal(this.element.children.length, 1);
+    assert.equal(this.element.querySelector('.qa-fact-value').innerText, 'lorem ipsum');
+    assert.equal(this.element.querySelector('.qa-fact-author').innerText, '— Someone about Chuck Norris');
+  });
+
+  module('without @data', function() {
+    test(`renders data's value`, async function(assert) {
+      await render(hbs`<Fact />`);
+
+      assert.equal(this.element.children.length, 0);
+    });
+  });
+});
