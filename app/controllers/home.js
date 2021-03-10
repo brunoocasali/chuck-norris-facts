fcc1977785be2e0dc8b04c095924181f61102850
Bruno Casali <brunoocasali@gmail.com>
2021-03-10 00:42:20 -0300

    Create first version of working app
    - Add logic to prevent duplicates
    - Add a way to load & present results in screen

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,6 +1,38 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+
+import ENV from 'chuck-norris-facts/config/environment';
 
 export default class HomeController extends Controller {
+  @tracked data;
+
+  constructor() {
+    super(...arguments);
+
+    this.random();
+  }
+
   async random() {
+    let counter = 0;
+    let unchanged = true;
+    let currentData = this.data;
+
+    do {
+      const response = await fetch(`${ENV.API}/jokes/random`, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json'
+        }
+      });
+
+      currentData = await response.json();
+
+      if (this.data?.id !== currentData?.id) {
+        unchanged = false;
+        this.data = currentData;
+      } else {
+        counter ++;
+      }
+    } while (unchanged && counter < 5);
   }
 }
--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -16,7 +16,79 @@ describe('Home', () => {
     cy.visit('/');
 
     cy.url().should('include', '/');
-
+    cy.get('button').should('exist');
     cy.get('title').contains('Home | Chuck Norris - Facts');
+  });
+
+  it('loads random fact automatically', () => {
+    mirage.create('fact');
+
+    cy.visit('/');
+
+    cy.get('.qa-fact').should('exist');
+    cy.get('.qa-fact > .qa-fact-value').should('not.be.empty');
+  });
+
+  context('when click on see more button', () => {
+    it('loads another random fact', () => {
+      mirage.createList('fact', 3);
+
+      cy.visit('/');
+      cy.get('.qa-fact-btn').click();
+
+      cy.get('.qa-fact').should('exist');
+      cy.get('.qa-fact > .qa-fact-value').should('not.be.empty');
+    });
+
+    it('guarantees that new fact will be different', () => {
+      const fact1 = mirage.create('fact', { value: 'fact 1' });
+      const fact2 = mirage.create('fact', { value: 'fact 2' });
+
+      let counter = 0;
+
+      mirage.get(`https://api.chucknorris.io/jokes/random`, () => {
+        // the idea is to force same response even in multiple callings
+        counter ++;
+
+        return [1, 2, 3].includes(counter) ? fact1 : fact2;
+      });
+
+      cy.visit('/');
+
+      cy.get('.qa-fact').should('exist');
+      cy.get('.qa-fact-value').contains('fact 1');
+
+      cy.get('.qa-fact-btn').click();
+
+      cy.get('.qa-fact-value').contains('fact 2');
+    });
+
+    it('does not retries more than 5 times', () => {
+      const fact1 = mirage.create('fact', { value: 'fact 1' });
+
+      let counter = 0;
+
+      mirage.logging = true
+      mirage.get(`https://api.chucknorris.io/jokes/random`, () => {
+        // the idea is to force same response even in multiple callings
+        counter ++;
+
+        // 1 (first call does not counts) + 5 retries
+        return fact1;
+      });
+
+      cy.visit('/');
+
+      cy.get('.qa-fact').should('exist');
+      cy.get('.qa-fact-value').contains('fact 1');
+
+      cy.get('.qa-fact-btn').click();
+
+      cy.get('.qa-fact-value').then(($h1) => {
+        cy.wrap($h1).contains('fact 1');
+
+        expect(counter).to.eq(6);
+      });
+    });
   });
 });
