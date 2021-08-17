839e5b84b5b8e5834f423df119627b9838272fc1
Bruno Casali <brunoocasali@gmail.com>
2021-08-17 10:49:39 -0300

    temp

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,23 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
+const environment = EmberApp.env();
+const isProd = environment === 'production';
+const isTest = environment === 'test';
+
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    hinting: isTest,
+    tests: isTest,
+    'ember-cli-babel': {
+      includePolyfill: isProd,
+    },
+    autoprefixer: {
+      sourcemap: false,
+    },
+    sourcemaps: {
+      enabled: isProd,
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -20,5 +34,24 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  const { Webpack } = require('@embroider/webpack');
+
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticComponents: true,
+    packagerOptions: {
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /\\.js?$/,
+              exclude: /mirage.*|cypress.*/,
+            },
+          ],
+        },
+      },
+    },
+  });
 };
